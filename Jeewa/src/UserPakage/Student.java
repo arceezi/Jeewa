86a7799e6332adbf8da24fb7736d5a0f96988816
Ralf <ralfcedric30@gmail.com>
2024-08-02 19:22:30 +0800

    starting

---
+++ Jeewa/src/UserPakage/Student.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UserPakage;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Student {
+    private int studentID;
+    private String name;
+    private ArrayList<Grade> grades;
+    
+    
+    
+    
+}
